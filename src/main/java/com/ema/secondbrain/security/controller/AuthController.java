63e206fc373bf2bc66574d49ebec87f5c711b995
Emanuele Cerilli <emanuele.cerilli@jc.elis.org>
2023-03-26 22:50:40 +0200

    minor fix

--- src/main/java/com/ema/secondbrain/security/controller/AuthController.java
+++ src/main/java/com/ema/secondbrain/security/controller/AuthController.java
@@ -19,7 +19,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
